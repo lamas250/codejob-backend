e4dd7efb54293577024d17a3115559dae6ee727e
lamas250 <igorlamas1994@gmail.com.br>
2020-09-29 20:00:07 -0300

    passport auth routes register,login,user,logout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware("auth:api")->get("/user", function (Request $request) {
-    return $request->user();
-});
 
+Route::prefix('auth')->group(function(){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware("auth:api");
+    Route::get('/user', [AuthController::class, 'user'])->middleware("auth:api");
+});
 
